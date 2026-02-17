469be38a22b3d16ca03c1bfaa351a43ae1add144
Lazaro Ticas <ticasle@masters.edu>
2026-02-16 21:05:25 -0800

    7th draft of test_dealer, with html file

--- test/test_dealer.py
+++ test/test_dealer.py
@@ -1,5 +1,13 @@
 from testing_base import *
 
 def test_dealCards():
-    dealR = Dealer()
+    deck = Deck()
+    dealR = Dealer(deck)
+    pl1 = Player('Player1')
+    pl2 = Player('Player2')
+    dealR.dealCards(2, [pl1, pl2])
+    print(pl1.showHand())
+    print(pl2.showHand())
+    
+
     return 
