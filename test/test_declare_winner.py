3a36e38d87971aa67547391e2066e2e89d813140
hannahulibarri <ulibarrihr@masters.edu>
2026-02-20 21:27:34 -0800

    BB-12: fix card input in testing

--- test/test_declare_winner.py
+++ test/test_declare_winner.py
@@ -1,17 +1,17 @@
 from testing_base import *
 def test_expected_winner():
     bob = Player("bob")
-    bob.chosen_card = Card("Ace of Spades", 1, [], [])
+    bob.chosen_card = Card("Spades", 1, [], [])
     alice = Player("alice")
-    alice.chosen_card = Card("King of Hearts", 13, [], [])
+    alice.chosen_card = Card("Hearts", 13, [], [])
     declare_winner(bob, alice)
     assert declare_winner(bob, alice) == alice
 
 def test_expected_tie():
     bob = Player("bob")
-    bob.chosen_card = Card("Ace of Spades", 1, [], [])
+    bob.chosen_card = Card("Spades", 1, [], [])
     alice = Player("alice")
-    alice.chosen_card = Card("Ace of Spades", 1, [], [])
+    alice.chosen_card = Card("Spades", 1, [], [])
     assert declare_winner(bob, alice) == None
 
 def test_handle_missing_cards():
@@ -21,6 +21,6 @@ def test_handle_missing_cards():
 
 def test_handle_missing_card():
     bob = Player("bob")
-    bob.chosen_card = Card("Ace of Spades", 1, [], [])
+    bob.chosen_card = Card("Spades", 1, [], [])
     alice = Player("alice")
     assert declare_winner(bob, alice) == None
