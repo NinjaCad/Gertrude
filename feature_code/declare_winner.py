0171b848f73ae26a10eb2233c81601c3a5fe6660
hannahulibarri <ulibarrihr@masters.edu>
2026-02-20 19:55:50 -0800

    BB-12: print result and return player

--- feature_code/declare_winner.py
+++ feature_code/declare_winner.py
@@ -3,8 +3,11 @@ def declare_winner(player1, player2):
     card1 = player1.chosen_card
     card2 = player2.chosen_card
     if card1.value > card2.value:
-        return "Player 1 wins with " + card1.name
+        print("Player 1 wins with " + card1.name)
+        return player1
     elif card2.value > card1.value:
-        return "Player 2 wins with " + card2.name
+        print("Player 2 wins with " + card2.name)
+        return player2
     else:
-        return "It's a tie! Both players have a(n)" + card1.name    
+        print("It's a tie! Both players have a(n)" + card1.name)
+        return None
