16a7e32e173637b215fc38bf90a1cf7055b30079
hannahulibarri <ulibarrihr@masters.edu>
2026-02-20 19:36:36 -0800

    BB-12: fix def function

--- feature_code/declare_winner.py
+++ feature_code/declare_winner.py
@@ -1,7 +1,7 @@
 from src.cardgames import *;
-function declare_winner(player1, player2):
-    card1 = player1.chosen_card;
-    card2 = player2.chosen_card;
+def declare_winner(player1, player2):
+    card1 = player1.chosen_card
+    card2 = player2.chosen_card
     if card1.value > card2.value:
         return "Player 1 wins with " + card1.name
     elif card2.value > card1.value:
