9a1a402fa73ccc15ea614c47e4c02738fd7d685f
hannahulibarri <ulibarrihr@masters.edu>
2026-02-20 21:22:27 -0800

    BB-12: fix import statements

--- src/declare_winner.py
+++ src/declare_winner.py
@@ -1,4 +1,4 @@
-from src.cardgames import Player  # Adjust the import path if necessary
+from cardgames.Player import Player
 def declare_winner(player1, player2):
     card1 = player1.chosen_card
     card2 = player2.chosen_card
@@ -8,9 +8,9 @@ def declare_winner(player1, player2):
     elif card2.value > card1.value:
         print("Player 2 wins with " + card2.name)
         return player2
-    else:
+    elif card1.value == card2.value & (card1.value != 0 | card2.value != 0):
         print("It's a tie! Both players have a(n)" + card1.name)
+        return player1, player2
+    else:
+        print("No winner could be determined. One or more players may be missing a card.")
         return None
-
-bob  = Player("Bob")
-alice = Player("Alice")
