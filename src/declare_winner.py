e6f2020fb5b0d8da34b3f719431e286a02b7dc7f
hannahulibarri <ulibarrihr@masters.edu>
2026-02-20 20:34:24 -0800

    BB-12: add declare_winner.py to src

---
+++ src/declare_winner.py
@@ -0,0 +1,16 @@
+from src.cardgames import Player  # Adjust the import path if necessary
+def declare_winner(player1, player2):
+    card1 = player1.chosen_card
+    card2 = player2.chosen_card
+    if card1.value > card2.value:
+        print("Player 1 wins with " + card1.name)
+        return player1
+    elif card2.value > card1.value:
+        print("Player 2 wins with " + card2.name)
+        return player2
+    else:
+        print("It's a tie! Both players have a(n)" + card1.name)
+        return None
+
+bob  = Player("Bob")
+alice = Player("Alice")
