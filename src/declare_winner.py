5b0b3c4421089cc21f6e4d3c4310b92a9de8694e
hannahulibarri <ulibarrihr@masters.edu>
2026-02-20 21:36:59 -0800

    BB-12: delete redundant else condition

--- src/declare_winner.py
+++ src/declare_winner.py
@@ -2,7 +2,10 @@ from cardgames.Player import Player
 def declare_winner(player1, player2):
     card1 = player1.chosen_card
     card2 = player2.chosen_card
-    if card1.value > card2.value:
+    if card1.value == 0 or card2.value == 0:
+        print("One or more players are missing a card. Cannot determine winner.")
+        return None
+    elif card1.value > card2.value:
         print("Player 1 wins!")
         return player1
     elif card2.value > card1.value:
@@ -11,6 +14,4 @@ def declare_winner(player1, player2):
     elif card1.value == card2.value & (card1.value != 0 | card2.value != 0):
         print("It's a tie!")
         return player1, player2
-    else:
-        print("No winner could be determined. One or more players may be missing a card.")
-        return None
+
