cd2c8ebfd857ab6743d54fe352b2cf586b899434
Lazaro Ticas <ticasle@masters.edu>
2026-02-22 17:39:16 -0800

    first iteration of Games.py, not fully completed

--- src/cardgames/Games.py
+++ src/cardgames/Games.py
@@ -6,13 +6,22 @@ class Games:
         self.deck = Deck()
 
     def main(self):
-        print('Welcome to the Games application!')
-        print('This games application is under development.')
-        
+        print('Welcome to the *insert name here*!')
+        amt = self.startGame(tr = True)        
         print('First 5 cards in standard 52-card deck:')
         for card in self.deck.cards[:5]:
             print(card)
         input('Press [Enter] to exit.')
+    
+    def startGame(self, tr):
+        while tr:
+            try:
+                amtPlayers = int(input("How many people are playing?"))
+                tr = False
+            except ValueError:
+                print("That doesn't make any sense, try again.")
+        print("out of startGame, returning to main")
+        return amtPlayers
 
 if __name__ == "__main__":
     game = Games()
