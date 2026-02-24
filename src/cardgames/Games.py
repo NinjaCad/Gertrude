97756d66c576ad93554f54c32508439ff1f81f9c
Derek Smith <smithda1@masters.edu>
2026-02-24 11:31:58 -0800

    GERT-13: slight changes

--- src/cardgames/Games.py
+++ src/cardgames/Games.py
@@ -10,10 +10,29 @@ class Games:
         print('This games application is under development.')
         
         print('First 5 cards in standard 52-card deck:')
-        for card in self.deck.cards[:5]:
-            print(card)
+        temp = 0
+        for card in self.deck.cards[:52]:
+            print(temp, card)
+            temp += 1
         input('Press [Enter] to exit.')
+
+# def aceLogic(hand):
+    
+#     ace = 0, 
+
+#     total = sum(hand)
+#     ace_count = hand.count(11)
+
+#     # Downgrade Aces from 11 to 1 if busted
+#     while total > 21 and ace_count > 0:
+#         total -= 10   # 11 → 1
+#         ace_count -= 1
+
+#     return total
 
 if __name__ == "__main__":
     game = Games()
     game.main()
+ 
+ 
+
