8af1b4290f5c7931d85654373e8f3e9820c1969c
Lazaro Ticas <ticasle@masters.edu>
2026-02-23 20:36:35 -0800

    GERT-20: added comments

--- src/cardgames/Games.py
+++ src/cardgames/Games.py
@@ -10,10 +10,12 @@ class Games:
 
     def main(self):
         print('Welcome to the *insert name here*!')
-        self.playerList = self.startGame(tr = True)        
+        self.playerList = self.startGame(tr = True)
+        #starts the game, output should be printed        
         self.gertDealer = Dealer(self.deck)
         for i in range(len(self.playerList)):
             self.gertDealer.dealCards(2, self.playerList)
+
             #may need to change the int 2 in the future,
             #not currently sure if we are going to do 1 card
             #and then another card and show the second
@@ -29,6 +31,7 @@ class Games:
         while tr:
             try:
                 self.amtPlayers = int(input("How many people are playing? "))
+                #include a minimum and maximum amount of players: https://www.w3schools.com/python/ref_keyword_raise.asp
                 tr = False
             except ValueError:
                 print("That doesn't make any sense, try again.")
@@ -36,6 +39,7 @@ class Games:
         self.pl_list = []
         for i in range(self.amtPlayers):
             self.pl_list.append(Player(input("Player {:d}'s name is: ".format(i))))
+        self.pl_list.append("GERTRUDE")
 
 
         return self.pl_list
