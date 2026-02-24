19a86b0c7a5a8d7aef685c1fd305970cbdedbec8
David Goree <goreeda@masters.edu>
2026-02-24 15:29:51 -0800

    Update Player.py

--- src/cardgames/Player.py
+++ src/cardgames/Player.py
@@ -5,29 +5,29 @@ class Player:
         self.name = name
         self.hand = []
         self.knownCards = []
-        self.idntity = ()
 
-    def addCard(self, card: Card, isKnown: bool = True):
+    # changed addCard function to default the isKnown attrib to False, but still retain
+    # some functionality if an explicit call to isKnown = True is needed for some reason.
+    def addCard(self, card: Card, isKnown: bool = False):
         self.hand.append(card)
-        if isKnown:
-            self.knownCards.append(True)
-        else:
-            self.knownCards.append(False)
+        self.knownCards.append(isKnown)
 
     def setHand(self, cards: "list[Card]", isKnown: bool = False):
         self.hand = cards
-        self.knownCards = [isKnown for _ in self.hand]
+        # remove the following line to leave all cards in hand at their default of "isKnown = False" 
+        #self.knownCards = [isKnown for _ in self.hand]
 
-    def showHand(self, printShort: bool = False):
-        for idx in range(6):
-            for i, card in enumerate(self.hand):
-                if printShort and i < len(self.hand)-1:
-                    image = card.shortImage[idx]    if self.knownCards[i] else card.cardBack[idx]
-                    print(image, end="")
-                else:
-                    image = card.image[idx] if self.knownCards[i] else card.cardBack[idx]
-                    print(image, end="")
-            print()
+    # Removed the showHand function because there's no need for it in our game.
+    # def showHand(self, printShort: bool = False):
+    #     for idx in range(6):
+    #         for i, card in enumerate(self.hand):
+    #             if printShort and i < len(self.hand)-1:
+    #                 image = card.shortImage[idx]    if self.knownCards[i] else card.cardBack[idx]
+    #                 print(image, end="")
+    #             else:
+    #                 image = card.image[idx] if self.knownCards[i] else card.cardBack[idx]
+    #                 print(image, end="")
+    #         print()
 
     def clearHand(self):
         self.hand = []
