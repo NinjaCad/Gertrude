2645ddcf4be65f1742a41425bb2f3476c33d1e81
Derek Smith <smithda1@masters.edu>
2026-02-24 12:39:23 -0800

    GERT-13: changes to games i think

--- src/cardgames/Games.py
+++ src/cardgames/Games.py
@@ -16,33 +16,9 @@ class Games:
             temp += 1
         input('Press [Enter] to exit.')
 
-def check.cards(pl, hand):
-      
-    card_values = {}
 
-    for i in range(52):
-        rank_index = i % 13  # 0=Ace, 1=Two ... 10=Jack, 11=Queen, 12=King
 
-        if rank_index == 0:
-            val = 11        # Ace
-        elif rank_index >= 10:
-            val = 10        # Jack, Queen, King
-        else:
-            val = rank_index + 1 # 2-10 (Index 1 is card '2', Index 9 is card '10')
-        
-    card_values[i] = val
 
-    card_values[0]
-
-    # total = sum(hand)
-    # ace_count = hand.count(11)
-
-    # # Downgrade Aces from 11 to 1 if busted
-    # while total > 21 and ace_count > 0:
-    #     total -= 10   # 11 → 1
-    #     ace_count -= 1
-
-    # return total
 
 if __name__ == "__main__":
     game = Games()
