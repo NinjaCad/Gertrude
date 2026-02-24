1108696360654e77df65b3253bed04b01ffd4e3b
Derek Smith <smithda1@masters.edu>
2026-02-24 12:29:55 -0800

    check_card function created

--- src/cardgames/Games.py
+++ src/cardgames/Games.py
@@ -16,19 +16,33 @@ class Games:
             temp += 1
         input('Press [Enter] to exit.')
 
-def aceLogic(pl, hand):
+def check.cards(pl, hand):
       
-    ace = [0, 13, 26, 39]
-      
-    total = sum(hand)
-    ace_count = hand.count(11)
+    card_values = {}
 
-    # Downgrade Aces from 11 to 1 if busted
-    while total > 21 and ace_count > 0:
-        total -= 10   # 11 → 1
-        ace_count -= 1
+    for i in range(52):
+        rank_index = i % 13  # 0=Ace, 1=Two ... 10=Jack, 11=Queen, 12=King
 
-    return total
+        if rank_index == 0:
+            val = 11        # Ace
+        elif rank_index >= 10:
+            val = 10        # Jack, Queen, King
+        else:
+            val = rank_index + 1 # 2-10 (Index 1 is card '2', Index 9 is card '10')
+        
+    card_values[i] = val
+
+    card_values[0]
+
+    # total = sum(hand)
+    # ace_count = hand.count(11)
+
+    # # Downgrade Aces from 11 to 1 if busted
+    # while total > 21 and ace_count > 0:
+    #     total -= 10   # 11 → 1
+    #     ace_count -= 1
+
+    # return total
 
 if __name__ == "__main__":
     game = Games()
