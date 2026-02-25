ca4fcd38c802f95bfdd2e1437a18cf931fbf9da5
SaakeJR <jeremiahsaake@gmail.com>
2026-02-24 18:14:02 -0800

    added checkForFourOfAKind function

--- src/cardgames/Player.py
+++ src/cardgames/Player.py
@@ -28,6 +28,25 @@ class Player:
                     print(image, end="")
             print()
 
+    def checkForFourOfAKind(self):
+        counts_list = (("Aces", 0), ("Twos", 0), ("Threes", 0), ("Fours", 0), ("Fives", 0), ("Sixes", 0), ("Sevens", 0), ("Eights", 0), ("Nines", 0), ("Tens", 0), ("Jacks", 0), ("Queens", 0), ("Kings", 0))
+        isFourOfAKind = []
+        numberOfFourOfAKindsInHand = 0
+        for card in self.hand:
+            index = card.value
+            (key, value) = counts_list[index-1]
+            value += 1
+        for  i in range(1,14):
+            (key, value) = counts_list[i-1]
+            if value == 4:
+                isFourOfAKind.append(key)
+                numberOfFourOfAKindsInHand += 1
+        if isFourOfAKind == []:
+            return False
+        else:
+            return isFourOfAKind, numberOfFourOfAKindsInHand
+
+
     def clearHand(self):
         self.hand = []
         self.knownCards = []
