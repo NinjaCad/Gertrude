3881768c09bae84410dd19ef6a14aee3cdd9df3a
SaakeJR <jeremiahsaake@gmail.com>
2026-02-24 23:44:44 -0800

    fixed test logic for checking for a 4 of a kind by adding functions for assertion to work, also added successful html test file

--- test/test_checkIfFourOfAKind.py
+++ test/test_checkIfFourOfAKind.py
@@ -20,34 +20,38 @@ for player in players.values():
     player.showHand(True)
     print()
 
-cardImages=[0]
-index=0
-cardBack=0
+    cardImages=[0]
+    index=0
+    cardBack=0
+def test_empty_hand_bookless():
+    #Has no cards
+    player.clearHand()
+    assert player.checkForFourOfAKind() == False
 
-#Has no cards
-player.clearHand()
-assert player.checkForFourOfAKind() == False
+def test_small_hand_bookless():
+    #Does not have enough cards
+    list_of_cards = [Card("Spades", 1, cardImages[index], cardBack), Card("Clubs", 1, cardImages[index], cardBack), Card("Hearts", 1, cardImages[index], cardBack)]
+    player.clearHand()
+    player.setHand(list_of_cards)
+    assert player.checkForFourOfAKind() == False
 
-#Does not have enough cards
-list_of_cards = [Card("Spades", 1, cardImages[index], cardBack), Card("Clubs", 1, cardImages[index], cardBack), Card("Hearts", 1, cardImages[index], cardBack)]
-player.clearHand()
-player.setHand(list_of_cards)
-assert player.checkForFourOfAKind() == False
+def test_big_hand_bookless():
+    #Has Enough cards but no Book
+    list_of_cards = [Card("Spades", 1, cardImages[index], cardBack), Card("Clubs", 1, cardImages[index], cardBack), Card("Hearts", 1, cardImages[index], cardBack), Card("Spades", 2, cardImages[index], cardBack), Card("Clubs", 2, cardImages[index], cardBack)]
+    player.clearHand()
+    player.setHand(list_of_cards)
+    assert player.checkForFourOfAKind() == False
 
-#Has Enough cards but no Book
-list_of_cards = [Card("Spades", 1, cardImages[index], cardBack), Card("Clubs", 1, cardImages[index], cardBack), Card("Hearts", 1, cardImages[index], cardBack), Card("Spades", 2, cardImages[index], cardBack), Card("Clubs", 2, cardImages[index], cardBack)]
-player.clearHand()
-player.setHand(list_of_cards)
-assert player.checkForFourOfAKind() == False
+def test_book_in_hand():
+    #Has A book
+    list_of_cards = (Card("Spades", 1, cardImages[index], cardBack), Card("Clubs", 1, cardImages[index], cardBack), Card("Hearts", 1, cardImages[index], cardBack), Card("Diamonds", 1, cardImages[index], cardBack), Card("Spades", 2, cardImages[index], cardBack))
+    player.clearHand()
+    player.setHand(list_of_cards)
+    assert player.checkForFourOfAKind() == (['Aces'], 1)
 
-#Has A book
-list_of_cards = (Card("Spades", 1, cardImages[index], cardBack), Card("Clubs", 1, cardImages[index], cardBack), Card("Hearts", 1, cardImages[index], cardBack), Card("Diamonds", 1, cardImages[index], cardBack), Card("Spades", 2, cardImages[index], cardBack))
-player.clearHand()
-player.setHand(list_of_cards)
-assert player.checkForFourOfAKind() == (['Aces'], 1)
-
-#Has 2 Books
-list_of_cards = (Card("Spades", 1, cardImages[index], cardBack), Card("Clubs", 1, cardImages[index], cardBack), Card("Hearts", 1, cardImages[index], cardBack), Card("Diamonds", 1, cardImages[index], cardBack), Card("Spades", 2, cardImages[index], cardBack), Card("Clubs", 2, cardImages[index], cardBack), Card("Hearts", 2, cardImages[index], cardBack), Card("Diamonds", 2, cardImages[index], cardBack))
-player.clearHand()
-player.setHand(list_of_cards)
-assert player.checkForFourOfAKind() == (['Aces', 'Twos'], 2)
+def test_books_in_hand():
+    #Has 2 Books
+    list_of_cards = (Card("Spades", 1, cardImages[index], cardBack), Card("Clubs", 1, cardImages[index], cardBack), Card("Hearts", 1, cardImages[index], cardBack), Card("Diamonds", 1, cardImages[index], cardBack), Card("Spades", 2, cardImages[index], cardBack), Card("Clubs", 2, cardImages[index], cardBack), Card("Hearts", 2, cardImages[index], cardBack), Card("Diamonds", 2, cardImages[index], cardBack))
+    player.clearHand()
+    player.setHand(list_of_cards)
+    assert player.checkForFourOfAKind() == (['Aces', 'Twos'], 2)
