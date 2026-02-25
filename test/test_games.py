e206487494a246c1447c9c77a36f94858df477c1
Caedmon Julian <caedmon.julian@gmail.com>
2026-02-24 22:52:08 -0800

    updated test file

--- test/test_games.py
+++ test/test_games.py
@@ -4,7 +4,20 @@ game = Games()
 players = []
 players.append(Player("Bob"))
 players.append(Player("Bobby"))
-players.append(Player("Bobette"))
-players.append(Player("Bombadino"))
 
 game.round(players)
+# test case 1:
+# prints: "Bob's hand: "
+# input: "stand" -> prints: "stand"
+# prints: "Bobby's hand: "
+# input: "stand" -> prints: "stand"
+#
+# test case 2:
+# prints: "Bob's hand: "
+# input: "hit" -> prints: "stand"
+# prints: "Bob's hand: "hit
+# input: "stand" -> prints: "stand"
+# prints: "Bobby's hand: "
+# input: "hit" -> prints: "hit"
+# prints: "Bobby's hand: "
+# input: "stand" -> prints: "stand"
