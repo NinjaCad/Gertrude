6cff9fe9d3060a03954bd81d0901940d70dee1bc
Lazaro Ticas <ticasle@masters.edu>
2026-02-24 21:30:21 -0800

    GERT-20: no changes, had issue

--- test/test_games.py
+++ test/test_games.py
@@ -4,6 +4,7 @@ game = Games()
 playerList = [Player("Player1"), Player("Player2"), Player("Player3")]
 
 
-def test_amountPlayers(monkeypatch):
+def test_amountPlayers():
+    #call startGame(), include instructions to input for each case that you're testing
     #https://stackoverflow.com/questions/35851323/how-to-test-a-function-with-input-call
     assert game.startGame(True) ==  Exception("That's too many players! Try again.")
