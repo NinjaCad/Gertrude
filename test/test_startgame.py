b087b3531b4154be07d67234934db0b16179aa1e
Isaac Schat <schatio@masters.edu>
2026-03-02 14:00:40 -0800

    Changed test_startgame.py to check all players in list for number of cards

--- test/test_startgame.py
+++ test/test_startgame.py
@@ -24,22 +24,24 @@ def test_2player():
     turn_list = game.start_game(players)
     assert len(turn_list) == 2
     print("Length of list = 2")
-    assert len(turn_list[0].hand) == 7
+    for player in turn_list:
+        assert len(player.hand) == 7
     print("Player has 7 cards in hand")
 
 def test_4player():
     game = Games()
     Dory = Player("Dory")
-    Marvin = Player("Marlin")
+    Marlin = Player("Marlin")
     Nemo = Player("Nemo")
     FishThatAteNemosFamily = Player("ThatGuy")
 
-    players = [Marvin, Dory, Nemo, FishThatAteNemosFamily]
+    players = [Marlin, Dory, Nemo, FishThatAteNemosFamily]
     turn_list = game.start_game(players)
 
     assert len(turn_list) == 4
     print("Length of list = 4")
-    assert len(turn_list[0].hand) == 5
+    for player in turn_list:
+        assert len(player.hand) == 5
     print("Player has 5 cards in hand")
 
 test_basic()
