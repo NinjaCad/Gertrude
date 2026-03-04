5dcc3f870567814ef2d538861fcbd76f6647a5e9
Javier Vasquez <vasqueqja1@masters.edu>
2026-03-03 20:17:53 -0800

    test/test_deckShuffle src/cardgames/Games.py test/tutorial.py

--- src/cardgames/Games.py
+++ src/cardgames/Games.py
@@ -1,16 +1,15 @@
 from cardgames.Deck import Deck
 
 class Games:
-
-    def __init__(self):
-        self.deck = Deck() #deck is created here; deck knows how, games decides when
-        self.deck.shuffle() #object.method() - games gets the shuffle ability from deck.py
-
+            
     def main(self):
         print('Welcome to the Games application!')
         print('This games application is under development.')
         
         print('First 5 cards in standard 52-card deck:')
+        self.deck = Deck() #deck is created here; deck knows how, games decides when
+        self.deck.shuffle() #object.method() - games gets the shuffle ability from deck.py
+
         for card in self.deck.cards[:5]:
             print(card)
         print('Press [Enter] to exit.')
