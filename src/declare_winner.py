dfb2b3d3629df4e35e39b294d4e3345b7e467411
hannahulibarri <ulibarrihr@masters.edu>
2026-03-05 19:58:18 -0800

    BB-12: Remove print statements from function

--- src/declare_winner.py
+++ src/declare_winner.py
@@ -2,15 +2,11 @@ from cardgames.Player import Player
 def declare_winner(player1, player2):
     card1 = player1.chosen_card.value
     card2 = player2.chosen_card.value
-    if card1 == 0 or card2 == 0:
-        print("One or more players are missing a card. Cannot determine winner.")
+    if card1 == 0 or card2 == 0: # someone is missing a card
         return None
-    elif card1.value > card2.value:
-        print("Player 1 wins!")
+    elif card1.value > card2.value: # player1 wins
         return player1
-    elif card2.value > card1.value:
-        print("Player 2 wins!")
+    elif card2.value > card1.value: # player2 wins
         return player2
-    elif card1.value == card2.value:
-        print("It's a tie!")
+    elif card1.value == card2.value: # tie
         return player1, player2
