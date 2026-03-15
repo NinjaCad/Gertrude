e1799f753c2876af446dba10ad55835930f2aa50
hannahulibarri <ulibarrihr@masters.edu>
2026-03-14 19:17:00 -0700

    BB-8: change card inputs and error handling

--- src/declare_winner.py
+++ src/declare_winner.py
@@ -1,12 +1,13 @@
 from cardgames.Player import Player
 def declare_winner(player1, player2):
-    card1 = player1.chosen_card.value
-    card2 = player2.chosen_card.value
-    if card1 == 0 or card2 == 0: # someone is missing a card
-        return None
-    elif card1.compare(card2)==1: # player1 wins
-        return player1
-    elif card1.compare(card2)==-1: # player2 wins
-        return player2
-    elif card1.compare(card2)==0: # tie
-        return player1, player2
+    card1 = player1.chosen_card
+    card2 = player2.chosen_card
+    try:
+        if card1.compare(card2)==1: # player1 wins
+            return player1
+        elif card1.compare(card2)==-1: # player2 wins
+            return player2
+        else:
+            return player1, player2
+    except TypeError: # tie
+        print("Error: Both players must have chosen a card to declare a winner.")
