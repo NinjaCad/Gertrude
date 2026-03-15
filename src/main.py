efa6200fabc8b802724eacf23138def9460fcc0c
hannahulibarri <ulibarrihr@masters.edu>
2026-03-14 19:20:37 -0700

    Bb-8: output winner with stand in code

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from src.cardgames.Player import *
-from src.cardgames.Deck import *
+from cardgames.Player import *
+from cardgames.Deck import *
 from cardgames.Card_Compare import *
 from cardgames.Deck import *
 from cardgames.Player import *
@@ -20,14 +20,16 @@ deck.shuffle()
 
 # deal cards to players
 dealer = Dealer(deck)
-dealer.dealCards([player1, player2])
+dealer.dealCards(3, [player1, player2])
 
 # player1 chooses a card
+# stand in code
+player1.chosen_card = player1.hand[0]
 
 # swap turn function
 
 # player2 chooses a card
-
-# declare_winner(player1, player2)
-
-
+# stand in code
+player2.chosen_card = player2.hand[0]
+winner = declare_winner(player1, player2)
+print("The winner is: ", winner)
