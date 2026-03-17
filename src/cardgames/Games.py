aace4ffcde75c69e43d4efc84de7beb53af85b81
Javier Vasquez <vasqueqja1@masters.edu>
2026-03-17 16:55:33 -0700

    added turn order logic as well as functionality to progress despite an empty deck

--- src/cardgames/Games.py
+++ src/cardgames/Games.py
@@ -66,10 +66,28 @@ class Games:
         game_running = True #game essentially runs forever. logic is needed to state when the game ends!!!!
         while game_running:
             for player in turn_list:
-                print(player.name)
+                player.isTurn = True
+                print(f"\n{player.name}'s turn")
         #for each player in the turn list:
             #that player takes a turn
-    
+                if len(self.dealer.deck.size) == 0:
+                    print("Draw pile is empty. Cannot pick up new cards.")
+                else:
+                    while True:
+                        choice = input("Do you want to draw a card? (y/n)").lower()
+                        if choice == 'y':
+                            card = self.dealer.deck.getCard()
+                            player.hand.append(card) #we're assuming the player has a hand
+                            print(f"You drew: {card}")
+                            break
+                        elif choice == 'n':
+                            break
+                        else:
+                            print("Invalid input. Please enter 'y' or 'n'.")
+                
+                #print("Next, choose a player to ask and a card value") would come next                    
+                #end of player's turn
+                player.isTurn = False
     
         input('Press [Enter] to exit.')
 
