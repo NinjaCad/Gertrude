a5a076f257875ec570b5654c08465545a041e3ff
Caleb Canham <canhamcj@masters.edu>
2026-03-17 23:58:20 +0000

    reversing earlier mistake

---
+++ src/cardgames/Dealer.py
@@ -0,0 +1,31 @@
+from cardgames.Card import Card
+from cardgames.Deck import Deck
+from cardgames.Player import Player
+
+class Dealer:
+    def __init__(self, deck: Deck):
+        self.deck = deck
+        self.deck.shuffle()
+
+    def printCards(self, cards: "list[Card]", showFront: bool, printShort: bool = True):
+        for idx in range(6):
+            for i, card in enumerate(cards):
+                if printShort and i < len(cards)-1:
+                    image = card.shortImage[idx] if showFront else card.cardBack[idx]
+                    print(image, end="")
+                else:
+                    image = card.image[idx] if showFront else card.cardBack[idx]
+                    print(image, end="")
+            print()
+
+    def dealCards(self, numCards: int, players: "list[Player]"):
+        if numCards * len(players) > self.deck.size:
+            return False
+        for player in players:
+            for _ in range(numCards):
+                player.addCard(self.deck.getCard())
+        return True
+
+    def resetDeck(self):
+        self.deck.reset()
+        self.deck.shuffle()
