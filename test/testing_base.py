02e5c1af3a6e72e395e5ad6a20e25727dd52b5ee
Joseph Scott <scottje@masters.edu>
2026-03-17 00:13:28 -0700

    Added testing for the first set of routes. To be continued

--- test/testing_base.py
+++ test/testing_base.py
@@ -10,7 +10,7 @@ if str(ROOT) not in sys.path:
 from cardgames.Card import Card
 from cardgames.Deck import Deck
 from cardgames.Dealer import Dealer
-from cardgames.Games import Games
+from cardgames.Games import app
 from cardgames.Player import Player
 
 def getCard( suit, value):
