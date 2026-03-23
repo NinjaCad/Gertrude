6e5a4ec5fa343d25982e2f9709a904959d914cf4
Prince Abbey <abbeyph@masters.edu>
2026-03-22 22:42:36 -0700

    Card.py proper display on the command line the results of each round

--- src/cardgames/Card.py
+++ src/cardgames/Card.py
@@ -1,13 +1,46 @@
 class Card:
-    def __init__(self, suit, value, image, cardBack):
-        self.cardBack = cardBack
-        self.suit = suit
+    def __init__(self, suit_inp, value):
+        suits = {
+    "S": {"symbol": "♠", "name": "Spades"},
+    "H": {"symbol": "♥", "name": "Hearts"},
+    "D": {"symbol": "♦", "name": "Diamonds"},
+    "C": {"symbol": "♣", "name": "Clubs"}
+}
+        if suit_inp == 'S':
+                self.suit = suits["S"]["symbol"]
+
+        elif suit_inp == 'H':
+            self.suit = suits["H"]["symbol"]
+
+        elif suit_inp == 'D':
+            self.suit = suits["D"]["symbol"]
+
+        elif suit_inp == 'C':
+            self.__class__suit = suits["C"]["symbol"]
+        
+        values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
         self.value = value
-        self.image = image
-        self.shortImage = []
-        if self.image:
-            for line in self.image:
-                self.shortImage.append(line[:4])
+        self.image = f"""\
++---------+
+| {self.value:<2}    {self.suit} |
+|         |
+|    {self.suit}    |
+|         |
+| {self.suit}    {self.value:>2} |
++---------+
+"""
+        self.cardBack = """\
++---------+
+|░░░░░░░░░|
+|░  ◇◇◇  ░|
+|░  ◇◇◇  ░|
+|░  ◇◇◇  ░|
+|░░░░░░░░░|
++---------+
+"""
+        self.shortImage = [f'{self.suit}{self.value}']
+
+        return self.shortImage, self.image
 
     def __str__(self, short: bool = False):
         return '\n'.join(self.shortImage if short else self.image)
@@ -17,4 +50,3 @@ class Card:
             return False
         return self.suit == other.suit and \
             self.value == other.value
-
