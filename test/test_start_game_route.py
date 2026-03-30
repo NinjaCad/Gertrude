3d71eb201dea98da8e6f9fe5755d0777141dde78
Rose Hilst <hilstre@masters.edu>
2026-03-30 16:29:57 -0700

    Added test case for start_game() route and updated testing_base.py to import * from Games.py

--- test/test_start_game_route.py
+++ test/test_start_game_route.py
@@ -8,6 +8,13 @@ def client():
     with app.test_client() as client:
         yield client
 
+#test status_code
 def test_start_game_route(client):
     response = client.get("/start_game")
     assert response.status_code == 200
+
+#test correct data in response
+def test_start_game_route_data(client):
+    #referenced https://stackoverflow.com/questions/69370708/how-can-i-use-pytest-to-test-routes-created-using-flask for help with creating this test
+    response = client.get("/start_game")
+    assert b'<button type="submit">Play Card</button>' in response.data
