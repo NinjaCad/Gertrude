a2dce95d1f03d31955c010cd41fda5b8c69bc5e8
hannahulibarri <ulibarrihr@masters.edu>
2026-03-30 14:33:46 -0700

    BB-26: first test function

--- test/test_mid_screen.py
+++ test/test_mid_screen.py
@@ -1,22 +1,16 @@
-from testing_base import *
+from testing_base import * 
+from cardgames.mid_screen import start_transition_screen
+import time
 
 def test_clear_inputs():
-    print("=== PLAYER 1 TURN ===")
-    secret_choice = input("Player 1, choose a secret card: ")
-
-    print(f"\n[DEBUG] Player 1 chose: {secret_choice}")
-    print("(This should disappear after clearing the screen)")
-    
-    start_transition_screen()
-
-    print("=== PLAYER 2 TURN ===")
-    print("If you can see Player 1's choice above, clearing FAILED.\n")
-
-    input("Player 2, press ENTER to continue...")
-
+    print("(This should disappear after entering 's' and after Player 1 clicks [ENTER])", flush=True)
+    if input("Press 's' to start transition screen script: ") != 's':
+        print("Test aborted.")
+        return
+    else:
+        start_transition_screen()
     print("\nTest complete.")
 
 if __name__ == "__main__":
     test_clear_inputs()
 
-test_clear_inputs()
