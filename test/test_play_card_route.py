13b64cb5e21fa309bfffd25d728cf80fd9d25a1a
Rose Hilst <hilstre@masters.edu>
2026-03-30 22:41:23 -0700

    Added comments for clarity in test file

--- test/test_play_card_route.py
+++ test/test_play_card_route.py
@@ -8,10 +8,13 @@ def client():
     with app.test_client() as client:
         yield client
 
+#test status_code
 def test_play_card(client):
     response = client.get("/play_card")
     assert response.status_code == 200
 
+#test correct data in response
 def test_play_card_route_data(client):
+    #referenced https://stackoverflow.com/questions/69370708/how-can-i-use-pytest-to-test-routes-created-using-flask for help with creating this test
     response = client.get("/play_card")
     assert b'<form method="POST">' in response.data
