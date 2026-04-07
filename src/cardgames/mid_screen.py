a7b1d0c7c0eb71187ac620a145515de62987d067
hannahulibarri <ulibarrihr@masters.edu>
2026-04-07 10:25:41 -0700

    BB-26: create helper function to separate input from timing

--- src/cardgames/mid_screen.py
+++ src/cardgames/mid_screen.py
@@ -4,6 +4,13 @@ import time
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def wait_for_player(num):
+    # Wait for next player
+    input(f"\nPlayer {num}: Press ENTER when you're ready...")
+
+    # Clear before next player sees anything
+    clear_screen()
+
 def start_transition_screen(num1, num2):
     # End current player's turn
     input(f"\nPlayer {num1}: Press ENTER to end your turn...")
@@ -18,10 +25,5 @@ def start_transition_screen(num1, num2):
     
     time.sleep(2)
 
-    # Wait for next player
-    input(f"\nPlayer {num2}: Press ENTER when you're ready...")
-
-    # Clear before next player sees anything
-    clear_screen()
-
+    wait_for_player(num2)
 start_transition_screen(4,5)
