6f20b6c97230c2bac0d04dc34f64b9a75958056a
Joseph Scott <scottje@masters.edu>
2026-04-16 20:26:05 -0700

    Removed unnecessary line from test file

--- test/sound_effect_test.py
+++ test/sound_effect_test.py
@@ -9,21 +9,18 @@ def client():
         yield client
 
 def test_page_2_has_audio(client):
-    response = client.get("/home")
     response = client.post("/", data={"player_name": "Joey"}, follow_redirects=True)
     html = response.data.decode()
     assert '<audio id="sfx"' in html
     assert 'preload="auto"' in html
 
 def test_play_sound_js_present_page_2(client):
-    response = client.get("/home")
     response = client.post("/", data={"player_name": "Joey"}, follow_redirects=True)
     html = response.data.decode()
     assert "function play_sound" in html
     assert "/static/sfx/" in html
 
 def test_page_3_has_audio(client):
-    response = client.get("/home")
     response = client.post("/", data={"player_name": "Joey"}, follow_redirects=True)
     response = client.get("/game")
     html = response.data.decode()
@@ -31,7 +28,6 @@ def test_page_3_has_audio(client):
     assert 'preload="auto"' in html
 
 def test_play_sound_js_present_page_3(client):
-    response = client.get("/home")
     response = client.post("/", data={"player_name": "Joey"}, follow_redirects=True)
     response = client.get("/game")
     html = response.data.decode()
